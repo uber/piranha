8c63401157879a61fabe6836e8660f58737f45b9
Lazaro Clapp <lazaro@uber.com>
2020-07-16 15:25:53 -0400

    [PiranhaJava] Fail hard in the presence of configuration errors.
    This makes PiranhaConfigurationException a runtime exception,
    and gets rid of the `disabled` checker state. In general, it doesn't
    make much sense to run Piranha with invalid configuration and
    it's better to show the issue to users as visibly as possible,
    since either way the tool behavior will be broken in this case.
    
    This resolves #76 and #77.

--- java/piranha/src/main/java/com/uber/piranha/XPFlagCleaner.java
+++ java/piranha/src/main/java/com/uber/piranha/XPFlagCleaner.java
@@ -130,7 +130,6 @@ public class XPFlagCleaner extends BugChecker
    */
   private boolean initialized = false;
 
-  private boolean disabled = false;
   @Nullable private ErrorProneFlags flags = null;
 
   private String xpFlagName = "_xpflag_dummy";
@@ -330,16 +329,12 @@ public class XPFlagCleaner extends BugChecker
   @Override
   public Description matchCompilationUnit(
       CompilationUnitTree compilationUnitTree, VisitorState visitorState) {
-    if (!initialized && !disabled) {
+    if (!initialized) {
       Preconditions.checkNotNull(
           flags,
           "The configuration-aware constructor should have been called at this point, and flags set to "
               + "a non-null value.");
-      try {
-        init(flags);
-      } catch (PiranhaConfigurationException pe) {
-        disabled = true;
-      }
+      init(flags);
     }
     if (countsCollected) {
       // Clear out this info
@@ -367,7 +362,7 @@ public class XPFlagCleaner extends BugChecker
       }
       MemberSelectTree mst = (MemberSelectTree) mit.getMethodSelect();
       String methodName = mst.getIdentifier().toString();
-      if (!disabled && configMethodProperties.containsKey(methodName)) {
+      if (configMethodProperties.containsKey(methodName)) {
         return getXPAPI(mit, configMethodProperties.get(methodName));
       }
     }
