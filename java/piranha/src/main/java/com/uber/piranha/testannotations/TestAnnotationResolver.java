1100c80209cdd34fd754b1a4eb079c0fefcfd920
Lazaro Clapp <lazaro@uber.com>
2021-04-12 14:18:18 -0400

    Minor last touches.

--- java/piranha/src/main/java/com/uber/piranha/testannotations/TestAnnotationResolver.java
+++ java/piranha/src/main/java/com/uber/piranha/testannotations/TestAnnotationResolver.java
@@ -41,6 +41,9 @@ public final class TestAnnotationResolver {
   private static final String UNPARSED_FIELD_VALUE =
       "$$$UNPARSED$$$9A1B0050-1BBD-420F-B6B3-AA0DB7D0AAE8";
 
+  private static final String TRUE_STR = "true";
+  private static final String FALSE_STR = "false";
+
   /**
    * testAnnotationSpecs is a map where key is the annotation name and the value is an object
    * encoding the specification of a testing annotations understood by Piranha, including how to
@@ -55,6 +58,8 @@ public final class TestAnnotationResolver {
     this.testAnnotationSpecs = testAnnotationSpecs;
   }
 
+  // Pre-parse the actual arguments of an annotation instance into a multi-map of
+  // "$fieldName" -> {val1, val2, ...}, before trying to match to an spec.
   private static ImmutableMultimap<String, AnnotationArgument> parseAnnotationArguments(
       AnnotationTree at) {
     ImmutableMultimap.Builder<String, AnnotationArgument> builder = ImmutableMultimap.builder();
@@ -103,15 +108,15 @@ public final class TestAnnotationResolver {
     return builder.build();
   }
 
+  // For a given spec and annotation instance pair, try to resolve the annotation to the spec
   private Optional<ResolvedTestAnnotation> attemptResolveSpec(
       TestAnnotationSpecRecord spec, AnnotationTree at, Tree parent, VisitorState state) {
     if (!at.getAnnotationType().toString().contains(spec.getAnnotationName())) {
       // For sanity, attempting to resolve an annotation with a spec without the same name will
-      // always return
-      // empty.
+      // always return empty.
       return Optional.empty();
     }
-    ImmutableMultimap<String, AnnotationArgument> fields = parseAnnotationArguments(at);
+    final ImmutableMultimap<String, AnnotationArgument> fields = parseAnnotationArguments(at);
     Set<String> relevantFieldParsingErrors = new HashSet<String>();
     // Process flag id(s)
     ImmutableList.Builder<AnnotationArgument> flagIdsBuilder = ImmutableList.builder();
@@ -120,11 +125,13 @@ public final class TestAnnotationResolver {
       // Annotation field reference
       if (!fields.containsKey(flagIdentifierOrField)) {
         // Annotation does not match the spec. That's fine, maybe another spec with the same name
-        // does.
+        // does, let's wait until we have seen if that isn't the case before reporting an error.
         return Optional.empty();
       }
       for (AnnotationArgument argument : fields.get(flagIdentifierOrField)) {
         if (argument.getValue().equals(UNPARSED_FIELD_VALUE)) {
+          // There is an error parsing the value of this field, let's wait to see if the rest of the
+          // annotation matches the specification's shape before we report that as an error.
           relevantFieldParsingErrors.add(
               String.format(
                   "Field %s is not of a type which can be parsed to a flag or list of flags.",
@@ -152,9 +159,9 @@ public final class TestAnnotationResolver {
                 treatedValueOrField));
       }
       AnnotationArgument argument = fields.get(treatedValueOrField).asList().get(0);
-      if (argument.getValue().equals("true")) {
+      if (TRUE_STR.equalsIgnoreCase(argument.getValue())) {
         treated = true;
-      } else if (argument.getValue().equals("false")) {
+      } else if (FALSE_STR.equalsIgnoreCase(argument.getValue())) {
         treated = false;
       } else {
         relevantFieldParsingErrors.add(
@@ -166,8 +173,9 @@ public final class TestAnnotationResolver {
     } else {
       // Only 'true' or 'false' are possible at this point, due to parsing logic
       Preconditions.checkArgument(
-          treatedValueOrField.equals("true") || treatedValueOrField.equals("false"));
-      treated = treatedValueOrField.equals("true");
+          TRUE_STR.equalsIgnoreCase(treatedValueOrField)
+              || FALSE_STR.equalsIgnoreCase(treatedValueOrField));
+      treated = TRUE_STR.equalsIgnoreCase(treatedValueOrField);
     }
     // Process treatment group, if any
     ImmutableList.Builder<AnnotationArgument> groupsBuilder = ImmutableList.builder();
@@ -182,7 +190,7 @@ public final class TestAnnotationResolver {
           String.format(
               "Found annotation %s at %s, matching the shape of the test annotation "
                   + "specification `%s` from Piranha's configuration file. However, one or more errors "
-                  + "occurred when parsing the annotation's fields:%s",
+                  + "occurred when parsing the annotation's fields:\n\t* %s",
               state.getSourceForNode(at),
               ASTHelpers.getSymbol(parent).flatName().toString(),
               spec,
@@ -231,7 +239,7 @@ public final class TestAnnotationResolver {
               String.format(
                   "Found annotation %s at %s, matching the name of a test annotation "
                       + "declared in Piranha's config file. However, the annotation can't be matched "
-                      + "to any of the specified shapes in this configuration. Candidates are:%s",
+                      + "to any of the specified shapes in this configuration. Candidates are:\n\t%s",
                   state.getSourceForNode(at),
                   ASTHelpers.getSymbol(tree).flatName().toString(),
                   candidateAnnotations
