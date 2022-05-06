9301dc9dd3fa5a8dd89429f531fc707df28aa464
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2022-05-06 09:14:05 -0700

    Updating the demo to the cmd line args

--- generic/piranha-tree-sitter/run_piranha_demo.sh
+++ generic/piranha-tree-sitter/run_piranha_demo.sh
@@ -11,4 +11,4 @@
 
 cargo build --release
 cp target/release/piranha-tree-sitter  demo/java/piranha-tree-sitter
-./demo/java/piranha-tree-sitter -c demo/java/ -f demo/java/configurations -p demo/java/demo_piranha_arguments.toml
+./demo/java/piranha-tree-sitter -c demo/java/ -f demo/java/configurations
