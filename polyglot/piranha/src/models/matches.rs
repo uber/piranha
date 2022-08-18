8aab1f1c2c6d6c553d8a11933b49a43b6f3c8d67
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2022-08-17 20:59:44 -0700

    Remove dirty hack. This hack makes things inflexible.

--- polyglot/piranha/src/models/matches.rs
+++ polyglot/piranha/src/models/matches.rs
@@ -13,69 +13,70 @@ Copyright (c) 2022 Uber Technologies, Inc.
 
 use std::collections::HashMap;
 
-use serde::{Serialize, Serializer};
-use tree_sitter::Range;
+use serde_derive::Serialize;
 
-#[derive(serde_derive::Serialize, Debug, Clone)]
+#[derive(Serialize, Debug, Clone)]
 pub(crate) struct Match {
   // Range of the entire AST node captured by the match
-  #[serde(serialize_with = "ser_range")]
   range: Range,
   // The mapping between tags and string representation of the AST captured.
   matches: HashMap<String, String>,
 }
 
 impl Match {
-  pub(crate) fn new(range: Range, matches: HashMap<String, String>) -> Self {
-    Self { range, matches }
+  pub(crate) fn new(range: tree_sitter::Range, matches: HashMap<String, String>) -> Self {
+    Self {
+      range: Range {
+        start_byte: range.start_byte,
+        end_byte: range.end_byte,
+        start_point: Point {
+          row: range.start_point.row,
+          column: range.start_point.column,
+        },
+        end_point: Point {
+          row: range.end_point.row,
+          column: range.end_point.column,
+        },
+      },
+      matches,
+    }
   }
 
   /// Get the edit's replacement range.
-  pub(crate) fn range(&self) -> Range {
-    self.range
+  pub(crate) fn range(&self) -> tree_sitter::Range {
+    tree_sitter::Range {
+      start_byte: self.range.start_byte,
+      end_byte: self.range.end_byte,
+      start_point: tree_sitter::Point {
+        row: self.range.start_point.row,
+        column: self.range.start_point.column,
+      },
+      end_point: tree_sitter::Point {
+        row: self.range.end_point.row,
+        column: self.range.end_point.column,
+      },
+    }
   }
 
   pub(crate) fn matches(&self) -> &HashMap<String, String> {
     &self.matches
   }
 }
-
-/// This method serializes the `tree_sitter::Range` struct. Originally, it does not derive serde::Serialize
-/// So in this method, we cast `Range` to a local type `LocalRange` and serialize this casted object.
-/// Note `LocalRange` derives serialize.
-fn ser_range<S: Serializer>(range: &Range, serializer: S) -> Result<S::Ok, S::Error> {
-  let local_range = LocalRange {
-    start_byte: range.start_byte,
-    end_byte: range.end_byte,
-    start_point: LocalPoint {
-      row: range.start_point.row,
-      column: range.start_point.column,
-    },
-    end_point: LocalPoint {
-      row: range.end_point.row,
-      column: range.end_point.column,
-    },
-  };
-
-  // Instead of serializing Vec<ExternalCrateColor>, we serialize Vec<LocalColor>.
-  local_range.serialize(serializer)
-}
-
 /// A range of positions in a multi-line text document, both in terms of bytes and of
 /// rows and columns.
 /// Note `LocalRange` derives serialize.
 #[derive(serde_derive::Serialize, Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct LocalRange {
+struct Range {
   start_byte: usize,
   end_byte: usize,
-  start_point: LocalPoint,
-  end_point: LocalPoint,
+  start_point: Point,
+  end_point: Point,
 }
 
 /// A range of positions in a multi-line text document, both in terms of bytes and of
 /// rows and columns.
 #[derive(serde_derive::Serialize, Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct LocalPoint {
+struct Point {
   row: usize,
   column: usize,
 }
