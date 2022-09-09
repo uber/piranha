c0bf492a1e08611afedda83da9229e57b4d93db7
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2022-09-09 07:37:13 -0700

    moving around stuff

--- polyglot/piranha/demo/stale_feature_flag_cleanup_demos.py
+++ polyglot/piranha/demo/stale_feature_flag_cleanup_demos.py
@@ -1,5 +1,12 @@
+from os.path import join, dirname
 from polyglot_piranha import run_piranha_cli
 
+dirname = dirname(__file__)
 
-output_summary = run_piranha_cli('java', 'java/configurations', True)
+output_summary_java = run_piranha_cli(join(dirname, 'java/ff'), join(dirname, 'java/ff/configurations'), True)
+
+
+output_summary_kt = run_piranha_cli(join(dirname, 'kt/ff'), join(dirname, 'kt/ff/configurations'), True)
+
+
 print()
