5ad5d59dffed1f3eeebf1f0d5d479fc632425211
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2022-09-19 21:16:21 -0700

    .

--- polyglot/piranha/test-resources/kt/file_scoped_chain_rules/expected/Sample.kt
+++ polyglot/piranha/test-resources/kt/file_scoped_chain_rules/expected/Sample.kt
@@ -1,9 +1,8 @@
 package com.uber.piranha
 
-import com.uber.something.piranha.ParameterUtils
-
 interface SceneStateParameters {
+    
     fun create(cachedParameters: CachedParameters): SceneStateParameters {
-      return ParameterUtils.create(SceneStateParameters::class.java, cachedParameters)
+      return  SceneStateParametersProvider.create(cachedParameters)
     }
-}
+  }
