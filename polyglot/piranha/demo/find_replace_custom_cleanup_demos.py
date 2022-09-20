74f8a86e34c3c63c7fd136c73d8318de7de9d413
Diego Marcilio <diego.marcilio@uber.com>
2022-09-20 04:24:29 +0200

    Onboarding structural find/replace for python (#248)

--- polyglot/piranha/demo/find_replace_custom_cleanup_demos.py
+++ polyglot/piranha/demo/find_replace_custom_cleanup_demos.py
@@ -16,10 +16,21 @@ def java_demo():
     info("Running the Find/Replace Custom Cleanup demo for Java")
     _ = run_piranha_cli(join(find_Replace_dir, "java"), join(find_Replace_dir, "java/configurations"), True)
 
+def python_demo():
+    """
+    Deletes the string literal `@str_literal` (from `substitutions` in `piranha_arguments.toml`) if it appears as a list element.
+    The string literal can be a single quotes or double quotes python string.
+    Also deletes an assignment to an empty list if @str_literal is the only element of the list.
+    The purpose of having `empty_list` is to demonstrate that the empty list assignment is only triggered as a consequence of the seed rule.
+    Finally, replaces the string literal `@str_to_replace` with `@str_replacement` (from `substitutions` in `piranha_arguments.toml`) if it appears as a list element.
+    """
+    print("Running the Find/Replace Custom Cleanup demo for Python")
+    _ = run_piranha_cli(join(find_Replace_dir, "python"), join(find_Replace_dir, "python/configurations"), True)
 
 FORMAT = '%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s'
 logging.basicConfig(format=FORMAT)
 logging.getLogger().setLevel(logging.INFO)
 
 java_demo()
+python_demo()
 info("Completed running the Find/Replace Custom Cleanup demos")
