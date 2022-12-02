c10f22b57869086bc3ec3e4bb638774f56b5ff8c
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2022-12-02 14:06:25 -0800

    cleanup

--- polyglot/piranha/src/utilities/tree_sitter_utilities.rs
+++ polyglot/piranha/src/utilities/tree_sitter_utilities.rs
@@ -337,17 +337,6 @@ pub(crate) fn get_replace_range(input_edit: InputEdit) -> Range {
   }
 }
 
-// #[cfg(test)]
-// use crate::models::language::PiranhaLanguage;
-// #[cfg(test)]
-// pub(crate) fn get_parser(language: PiranhaLanguage) -> Parser {
-//   let mut parser = Parser::new();
-//   parser
-//     .set_language(*language.language())
-//     .expect("Could not set the language for the parser.");
-//   parser
-// }
-
 #[cfg(test)]
 #[path = "unit_tests/tree_sitter_utilities_test.rs"]
 mod tree_sitter_utilities_test;
