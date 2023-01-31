1aa1491bdca1984909976681cedb2c8061a60874
Satyam Shubham <52656664+Satyam1749@users.noreply.github.com>
2023-02-01 05:21:48 +0530

    Set up test-resources and rust test for swift cleanup rules tests. (#340)

--- src/tests/test_piranha_swift.rs
+++ src/tests/test_piranha_swift.rs
@@ -37,4 +37,11 @@ create_rewrite_tests! {
     cleanup_comments = true,
     global_tag_prefix ="universal_tag.".to_string(),
     cleanup_comments_buffer = 3, delete_file_if_empty= false;
+
+    test_cleanup_rules_file: "cleanup_rules", 1,
+      substitutions = substitutions! {
+        "stale_flag" => "stale_flag_one"
+      },
+      cleanup_comments = true, delete_file_if_empty= false;
+
 }
