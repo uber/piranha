1ac944cd3e6629305ac6889e260dd37262d0da7d
satyam1749 <satyam.s@uber.com>
2023-01-31 13:57:07 +0530

    Cleaned up name in sample class

--- demo/feature_flag_cleanup/swift/sampleClass.swift
+++ demo/feature_flag_cleanup/swift/sampleClass.swift
@@ -1,19 +1,19 @@
 class SampleClass {
     init() {
         // Code 1
-        if TestEnum.stale_flag.isEnabled && abcd() {
+        if TestEnum.stale_flag.isEnabled && dummy_func_1() {
             // Code 2
-        } else if abcd() && TestEnum.stale_flag.isEnabled {
+        } else if dummy_func_1() && TestEnum.stale_flag.isEnabled {
             // Code 2
-        } else if !TestEnum.stale_flag.isEnabled && abcd() {
+        } else if !TestEnum.stale_flag.isEnabled && dummy_func_1() {
             // Code 2
         } else if TestEnum.stale_flag && !true {
             // Code 3
         } else if TestEnum.stale_flag.type && !false {
             // code 4
-        } else if TestEnum.stale_flag.isEnabled || abcd() {
+        } else if TestEnum.stale_flag.isEnabled || dummy_func_1() {
             // code 7
-        } else if def || TestEnum.stale_flag.isEnabled {
+        } else if dummy_var_1 || TestEnum.stale_flag.isEnabled {
             // code 8
         } else if TestEnum.staled_flag {
             // code 9
@@ -22,17 +22,17 @@ class SampleClass {
         } 
         // Code 11
 
-        let v1 = false && abcd()
-        let v2 = abcd() && false 
+        let v1 = false && dummy_func_1()
+        let v2 = dummy_func_1() && false 
 
         if TestEnum.stale_flag.isEnabled {
-            abcd()
+            dummy_func_1()
         } else if TestEnum.stale_flag.isEnabled{
-            efg()
+            dummy_func_2()
         } else if TestEnum.stale_flag.isEnabled{
-            hij()
+            dummy_func_3()
         } else {
-            klm()
+            dummy_func_4()
         }
     }
 }
