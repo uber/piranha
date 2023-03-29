e7ab9b951407c04b0aa4a5c5bd91463e5ba52b67
Adarsh Pandey <adarsh.narayan@uber.com>
2023-03-29 10:57:11 +0530

    Var Inline: Tests failing

--- test-resources/swift/cleanup_rules/input/VariableInlining.swift
+++ test-resources/swift/cleanup_rules/input/VariableInlining.swift
@@ -1,13 +1,13 @@
-// // Copyright (c) 2022 Uber Technologies, Inc.
-// // 
-// // <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
-// // except in compliance with the License. You may obtain a copy of the License at
-// // <p>http://www.apache.org/licenses/LICENSE-2.0
-// // 
-// // <p>Unless required by applicable law or agreed to in writing, software distributed under the
-// // License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-// // express or implied. See the License for the specific language governing permissions and
-// // limitations under the License.
+// // // Copyright (c) 2022 Uber Technologies, Inc.
+// // // 
+// // // <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+// // // except in compliance with the License. You may obtain a copy of the License at
+// // // <p>http://www.apache.org/licenses/LICENSE-2.0
+// // // 
+// // // <p>Unless required by applicable law or agreed to in writing, software distributed under the
+// // // License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// // // express or implied. See the License for the specific language governing permissions and
+// // // limitations under the License.
 
 class C21{
     func f211(){
@@ -53,18 +53,7 @@ class C26{
     }
 }
 
-class C1{
-    func fa1(){
-        var a = true
-        var b = false
-        if a {
-            doSomething()
-        }
-        if b {
-            doSomethingElse()
-        }
-    }
-}
+
 
 class C11{
     var a = true
@@ -87,6 +76,19 @@ class C12{
             doSomething()
         }
         if self.b {
+            doSomethingElse()
+        }
+    }
+}
+
+class C1{
+    func fa1(){
+        var a = true
+        var b = false
+        if a {
+            doSomething()
+        }
+        if b {
             doSomethingElse()
         }
     }
@@ -217,5 +219,20 @@ class C71{
         if a {
             var b = false
         }
+    }
+}
+
+class C7{
+    var a = true
+
+    func f1(){
+        f.subscribe(
+            onNext: { x in 
+                var b = false
+                if b {
+                    doSomething()
+                }
+            }
+        )
     }
 }
