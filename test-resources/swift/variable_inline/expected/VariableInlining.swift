0f5d2e0bb7dc4f58ace9f50acee54f069b004a38
Adarsh Pandey <adarsh.narayan@uber.com>
2023-04-03 15:15:27 +0530

    Variable Inlining [2/n]: Field Variable Inlining
    [ghstack-poisoned]

--- test-resources/swift/variable_inline/expected/VariableInlining.swift
+++ test-resources/swift/variable_inline/expected/VariableInlining.swift
@@ -62,16 +62,172 @@ class C6 {
 }
 
 class C7 {
-    func f7(){
+    func f7a(){
         
         if true {
             doSomething()
         }
     }
 
+    func f7b(){
+        
+        
+        if true {
+            doSomething()
+        }
+    }
+}
+
+class C8{
+    
+
+    init(){}
+
     func f8(){
         
+    }
+}
+
+class C9{
+    var a = true
+
+    init(){}
+
+    func f9(){
+        a = false
+    }
+}
+
+class C10{
+    
+
+    init(){}
+
+    func f10(){
         
+    }
+}
+
+class C11{
+    var a = true
+
+    init(){}
+
+    func f11(){
+        self.a = false
+    }
+}
+
+class C12{
+    
+    
+    func f12(){
+        if true {
+            doSomething()
+        }
+        if false {
+            doSomethingElse()
+        }
+    }
+}
+
+class C13{
+    
+    
+    func f13(){
+        if true {
+            doSomething()
+        }
+        if false {
+            doSomethingElse()
+        }
+    }
+}
+
+class C14{
+    
+
+    func f14(){
+        if true {
+            doSomething()
+        }
+    }
+}
+
+class C15{
+    
+
+    func f15(){
+        if true {
+            doSomething()
+        }
+    }
+}
+
+class C16{
+    var a = true
+
+    func f4(){
+        a = false
+    }
+
+    func f16(){
+        if a {
+            doSomething()
+        }
+    }
+}
+
+class C17{
+    var a = true
+
+    func f17a(){
+        self.a = false
+    }
+
+    func f17b(){
+        if self.a {
+            doSomething()
+        }
+    }
+}
+
+class C18{
+    var a = true
+
+    func f18a(){
+        a = true
+    }
+
+    func f18b(){
+        a = false
+    }
+}
+
+class C19{
+    var a
+    init(){
+        a = true
+        if a {
+            doSomething()
+        }
+    }
+
+    func f19(){
+        a = false
+    }
+}
+
+class C20{
+    
+    init(){
+        
+        if true {
+            doSomething()
+        }
+    }
+
+    func f20(){
         if true {
             doSomething()
         }
