4c8c4f5bc14806437f60e0f8e3fdebc1011d0903
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-04-03 17:07:11 -0700

    .

--- test-resources/swift/leading_comma/expected/Sample.swift
+++ test-resources/swift/leading_comma/expected/Sample.swift
@@ -2,10 +2,11 @@ enum TestEnum {
    case one
    case two
    case three
+   case four
 
    var v1: String {
       switch self {
-      case .two:
+      case .two, .four:
          return "Hello"
       case .three:
          return "Hi"
--- test-resources/swift/leading_comma/input/Sample.swift
+++ test-resources/swift/leading_comma/input/Sample.swift
@@ -2,10 +2,11 @@ enum TestEnum {
    case one
    case two
    case three
+   case four
 
    var v1: String {
       switch self {
-      case .two, .one:
+      case .two, .one, .four:
          return "Hello"
       case .three:
          return "Hi"
