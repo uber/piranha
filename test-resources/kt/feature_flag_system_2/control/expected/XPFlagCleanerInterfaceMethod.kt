9f5ddf1cf700156ba4754252f0287ba2c0fca951
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-04-04 13:50:13 -0700

    Update the license to 2023

--- test-resources/kt/feature_flag_system_2/control/expected/XPFlagCleanerInterfaceMethod.kt
+++ test-resources/kt/feature_flag_system_2/control/expected/XPFlagCleanerInterfaceMethod.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022 Uber Technologies, Inc.
+ * Copyright (c) 2023 Uber Technologies, Inc.
  *
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
