713635b5e56c5047eeaab35312df95195aec837f
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-04-25 11:30:04 -0700

    Update thrift_demos.py
    Format file

--- demo/thrift_demos.py
+++ demo/thrift_demos.py
@@ -6,6 +6,7 @@ from logging import info
 
 find_Replace_dir = join(dirname(__file__), "find_replace")
 
+
 def thrift_demo():
 
     file_path = join(find_Replace_dir, "thrift", "sample.thrift")
@@ -27,41 +28,42 @@ def thrift_demo():
             "input_exception_name", "rpc_code"
         }
     )
-    error_code_mapping = {  "Invalid" : "INVALID_ARGUMENT",
-                            "BadRequest" : "INVALID_ARGUMENT",
-                            "UserNotAllowed" : "INVALID_ARGUMENT",
-                            "Precondition" : "FAILED_PRECONDITION",
-                            "Range" : "OUT_OF_RANGE",
-                            "Unauthenticated" : "UNAUTHENTICATED",
-                            "Permission" : "PERMISSION_DENIED",
-                            "NotFound" : "NOT_FOUND",
-                            "DoesNotExist" : "NOT_FOUND",
-                            "Aborted" : "ABORTED",
-                            "Exists" : "ALREADY_EXISTS",
-                            "Exhausted" : "RESOURCE_EXHAUSTED",
-                            "Cancelled" : "CANCELLED",
-                            "DataLoss" : "DATA_LOSS",
-                            "UnknownError" : "UNKNOWN",
-                            "UnknownException" : "UNKNOWN",
-                            "InternalError" : "INTERNAL",
-                            "InternalException" : "INTERNAL",
-                            "ServiceError" : "INTERNAL",
-                            "ServiceException" : "INTERNAL",
-                            "ServerError" : "INTERNAL",
-                            "ServerException" : "INTERNAL",
-                            "Unimplemented" : "UNIMPLEMENTED",
-                            "Unavailable" : "UNAVAILABLE",
-                            "DeadlineExceeded" : "DEADLINE_EXCEEDED",
-                            }
-    for k, v in error_code_mapping.items() :
+    error_code_mapping = {"Invalid": "INVALID_ARGUMENT",
+                          "BadRequest": "INVALID_ARGUMENT",
+                          "UserNotAllowed": "INVALID_ARGUMENT",
+                          "Precondition": "FAILED_PRECONDITION",
+                          "Range": "OUT_OF_RANGE",
+                          "Unauthenticated": "UNAUTHENTICATED",
+                          "Permission": "PERMISSION_DENIED",
+                          "NotFound": "NOT_FOUND",
+                          "DoesNotExist": "NOT_FOUND",
+                          "Aborted": "ABORTED",
+                          "Exists": "ALREADY_EXISTS",
+                          "Exhausted": "RESOURCE_EXHAUSTED",
+                          "Cancelled": "CANCELLED",
+                          "DataLoss": "DATA_LOSS",
+                          "UnknownError": "UNKNOWN",
+                          "UnknownException": "UNKNOWN",
+                          "InternalError": "INTERNAL",
+                          "InternalException": "INTERNAL",
+                          "ServiceError": "INTERNAL",
+                          "ServiceException": "INTERNAL",
+                          "ServerError": "INTERNAL",
+                          "ServerException": "INTERNAL",
+                          "Unimplemented": "UNIMPLEMENTED",
+                          "Unavailable": "UNAVAILABLE",
+                          "DeadlineExceeded": "DEADLINE_EXCEEDED",
+                          }
+    for k, v in error_code_mapping.items():
         args = PiranhaArguments(
             language="thrift",
             path_to_codebase=file_path,
             rule_graph=RuleGraph(rules=[r1], edges=[]),
-            substitutions={"input_exception_name" : k, "rpc_code" : v}
+            substitutions={"input_exception_name": k, "rpc_code": v}
         )
         output = execute_piranha(args)
         print(output)
+
 
 FORMAT = "%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s"
 logging.basicConfig(format=FORMAT)
