dbea0ca554400efe3b9d668105ec2ccb649c9067
Adarsh Pandey <adarsh.narayan@uber.com>
2023-05-01 21:43:44 +0530

    Var Inline [3/n]: Ad hoc var inline
    [ghstack-poisoned]

---
+++ test-resources/swift/variable_inline/adhoc_variable_inline/expected/AdhocVariableInlining.swift
@@ -0,0 +1,22 @@
+class C1{
+    func f1(){
+        f.subscribe(
+            onNext: { x in 
+                
+            }
+        )
+    }
+}
+
+class C2{
+    var a
+
+    init(){
+        if something{
+            a = true
+            doSomething()
+        } else {
+            a = false
+        }
+    }
+}
