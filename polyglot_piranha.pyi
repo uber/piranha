63b1fdad67233b946923a391185237028a8fb600
Daniel Ramos <daniel.r.ramos@icloud.com>
2023-05-17 08:58:28 -0700

    Update polyglot_piranha.pyi
    Co-authored-by: Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>

--- polyglot_piranha.pyi
+++ polyglot_piranha.pyi
@@ -162,7 +162,7 @@ class Filter:
     """ A class to capture filters of a Piranha Rule
     """
     enclosing_node: TSQuery
-    "Scope in which the filter query has to be applied"
+    "AST patterns that some ancestor node of the primary match should comply"
     not_contains: list[TSQuery]
     "The Tree-sitter queries that need to be applied in the `enclosing_node` scope"
 
