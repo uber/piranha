a71bc4fbc01312dfcb9d2ac6ee3c52ffc25b28aa
Daniel Ramos <danielrr@uber.com>
2023-05-19 10:06:36 -0700

    Add back tests

--- src/models/unit_tests/source_code_unit_test.rs
+++ src/models/unit_tests/source_code_unit_test.rs
@@ -201,7 +201,73 @@ fn test_satisfies_filters_contains_positive() {
 }
 
 #[test]
-fn test_satisfies_filters_negative() {
+fn test_satisfies_filters_not_contains_positive() {
+  let _rule = piranha_rule! {
+    name= "test",
+    query= "(
+      ((local_variable_declaration
+                      declarator: (variable_declarator
+                                          name: (_) @variable_name
+                                          value: [(true) (false)] @init)) @variable_declaration)
+      )",
+    replace_node= "variable_declaration",
+    replace= "",
+    filters= [filter!{
+      enclosing_node= "(method_declaration) @md",
+      not_contains= ["(
+        ((assignment_expression
+                        left: (_) @a.lhs
+                        right: (_) @a.rhs) @assignment)
+        (#eq? @a.lhs \"@variable_name\")
+        (#not-eq? @a.rhs \"@init\")
+      )",]
+    }]
+
+  };
+  let rule = InstantiatedRule::new(&_rule, &HashMap::new());
+  let source_code = "class Test {
+      public void foobar(){
+        boolean isFlagTreated = true;
+        isFlagTreated = true;
+        if (isFlagTreated) {
+        // Do something;
+        }
+       }
+      }";
+
+  let mut rule_store = RuleStore::default();
+  let java = get_java_tree_sitter_language();
+  let mut parser = java.parser();
+  let piranha_args = PiranhaArgumentsBuilder::default()
+    .path_to_codebase(UNUSED_CODE_PATH.to_string())
+    .language(java)
+    .build();
+  let source_code_unit = SourceCodeUnit::new(
+    &mut parser,
+    source_code.to_string(),
+    &HashMap::new(),
+    PathBuf::new().as_path(),
+    &piranha_args,
+  );
+
+  let node = &source_code_unit
+    .root_node()
+    .descendant_for_byte_range(50, 72)
+    .unwrap();
+
+  assert!(source_code_unit.is_satisfied(
+    *node,
+    &rule,
+    &HashMap::from([
+      ("variable_name".to_string(), "isFlagTreated".to_string()),
+      ("init".to_string(), "true".to_string())
+    ]),
+    &mut rule_store,
+  ));
+}
+
+#[test]
+fn test_satisfies_filters_not_contains_negative() {
   let _rule = piranha_rule! {
     name= "test",
     query= "(
