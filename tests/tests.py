a64d3364eb3ca113e0da75a762c9f77e79bb3023
Daniel Ramos <daniel.r.ramos@icloud.com>
2023-05-19 15:11:55 -0700

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -8,7 +8,6 @@
 # License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
 from pathlib import Path
 from polyglot_piranha import Filter, execute_piranha, PiranhaArguments, PiranhaOutputSummary, Rule, RuleGraph, OutgoingEdges
 from os.path import join, basename
