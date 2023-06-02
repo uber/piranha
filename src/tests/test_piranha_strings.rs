a5d83f25c98669b931afeff440af5299df8c9f52
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-06-02 09:22:55 -0700

    Add support for strings

---
+++ src/tests/test_piranha_strings.rs
@@ -0,0 +1,35 @@
+/*
+ Copyright (c) 2023 Uber Technologies, Inc.
+
+ <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ except in compliance with the License. You may obtain a copy of the License at
+ <p>http://www.apache.org/licenses/LICENSE-2.0
+
+ <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied. See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+/*
+ Copyright (c) 2023 Uber Technologies, Inc.
+ <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ except in compliance with the License. You may obtain a copy of the License at
+ <p>http://www.apache.org/licenses/LICENSE-2.0
+ <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied. See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+use crate::models::default_configs::STRINGS;
+
+use super::{create_rewrite_tests, substitutions};
+
+create_rewrite_tests! {
+    STRINGS,
+    test_strings_scenario_rules_with_holes: "sample_strings/",1,
+    substitutions=  substitutions! {
+        "stale_key" => "Make Rich Text"
+      };
+}
