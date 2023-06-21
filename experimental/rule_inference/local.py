d5b163e32e8ca7625211f0daf9f95bc5aab38232
Daniel Ramos <danielrr@uber.com>
2023-06-21 12:39:40 -0700

    Formatting w prettier

--- experimental/rule_inference/local.py
+++ experimental/rule_inference/local.py
@@ -1,45 +1,80 @@
 import os
-
-from flask import Flask, request, jsonify
+import attr
+from flask import Flask, request, jsonify, session, render_template
+from flask_socketio import SocketIO, join_room
 import openai
-from flask import Flask, render_template
 import logging
 from piranha_agent import PiranhaAgent
 
 
+# Define data validation classes
+@attr.s
+class InferData:
+    source_code = attr.ib(validator=attr.validators.instance_of(str))
+    target_code = attr.ib(validator=attr.validators.instance_of(str))
+    language = attr.ib(validator=attr.validators.in_(["Python", "Java"]))
+    hints = attr.ib(validator=attr.validators.instance_of(str))
+
+
+@attr.s
+class FolderData:
+    folder_path = attr.ib(validator=attr.validators.instance_of(str))
+
+
+# Initialize Flask and SocketIO
 logging.getLogger("Flask").setLevel(logging.DEBUG)
 app = Flask(__name__)
+socketio = SocketIO(app)
+openai.api_key = os.getenv("OPENAI_API_KEY")  # Set API key at start
 
 
+# Routes
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/api/infer_piranha", methods=["POST"])
-def infer_from_example():
-    data = request.get_json()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+@socketio.on("infer_piranha")
+def infer_from_example(data):
+    try:
+        data = InferData(**data)
+    except (TypeError, ValueError) as e:
+        return {"error": str(e)}
+
     agent = PiranhaAgent(
-        data["source_code"],
-        data["target_code"],
-        language=data["language"],
-        hints=data["hints"],
+        data.source_code,
+        data.target_code,
+        language=data.language,
+        hints=data.hints,
     )
 
-    rule_name, rule = agent.infer_rules()
-    return jsonify(rule_name, rule), 200
+    room = session.get("room")
+    join_room(room)
+
+    try:
+        while True:  # continuously emit results
+            rule_name, rule = agent.infer_rules()
+            socketio.emit(
+                "infer_result", {"rule_name": rule_name, "rule": rule}, room=room
+            )
+            socketio.sleep(1)  # Sleep for a second to prevent overloading the client
+    except Exception as e:
+        socketio.emit("infer_error", {"error": str(e)}, room=room)
+
+    return {
+        "message": "Inference started"
+    }  # Acknowledge event reception and start of inference
 
 
 @app.route("/api/process_folder", methods=["POST"])
 def process_folder():
-    data = request.get_json()
-    folder_path = data.get("folder_path", "")
+    try:
+        data = FolderData(**request.get_json())
+    except (TypeError, ValueError) as e:
+        return jsonify({"error": str(e)}), 400
 
-    # Use the folder_path variable to process the folder.
-    # Note: This assumes your server has the appropriate permissions to access and read the directory.
-
-    # Let's just return a message for this example
+    folder_path = data.folder_path
+    # TODO: Process the folder
     return jsonify({"message": f"Received folder path: {folder_path}"}), 200
 
 
--- experimental/rule_inference/static/script.js
+++ experimental/rule_inference/static/script.js
@@ -10,7 +10,7 @@ let tree;
   const codeIntputAfter = document.getElementById("code-input-after");
   const languageSelect = document.getElementById("language-select");
   const queryInput = document.getElementById("query-input");
-  const explanation_input= document.getElementById('explanation-input');
+  const explanation_input = document.getElementById("explanation-input");
   languageSelect.addEventListener("change", handleLanguageChange);
   handleLanguageChange();
 
@@ -62,65 +62,69 @@ let tree;
     });
   }
 
+  document
+    .getElementById("submit-folder-button")
+    .addEventListener("click", async function () {
+      const folderPath = document.getElementById("folder-input").value; // Get the value of the folder path input
 
-  document.getElementById("submit-folder-button").addEventListener("click", async function () {
-    const folderPath = document.getElementById("folder-input").value; // Get the value of the folder path input
+      const response = await fetch("/api/process_folder", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          folder_path: folderPath, // Include the folder path in the request body
+        }),
+      });
 
-    const response = await fetch("/api/process_folder", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        folder_path: folderPath, // Include the folder path in the request body
-      }),
+      // handle response...
     });
 
-    // handle response...
-  });
+  // First, we need to establish a socket connection.
+  const socket = io.connect("http://127.0.0.1:5000");
 
-
-
-  document.getElementById("submit-button").addEventListener("click", async function () {
-    const sourceCode = codeBefore.getValue();
-    const targetCode = codeAfter.getValue();
-    const userExplanation = explanationEditor.getValue();
-    const language = languageSelect.value;
+  // We can listen for the 'infer_result' event and react when it happens.
+  socket.on("infer_result", function (data) {
+    // This is where you could update your interface with the data.
+    const toml = data.rule;
+    document.getElementById("query-container").style.display = "block";
+    queryEditor.setValue(toml);
 
     // Change the button to show that processing is happening
-    const button = document.getElementById('submit-button');
-    const buttonText = document.getElementById('button-text');
-    const spinner = document.getElementById('spinner');
+    const button = document.getElementById("submit-button");
+    const buttonText = document.getElementById("button-text");
+    const spinner = document.getElementById("spinner");
 
-    button.disabled = true; // Disable button
-    spinner.style.display = 'inline-block'; // Show spinner
-    buttonText.textContent = 'Processing...'; // Change button text
+    button.disabled = false; // Enable button
+    spinner.style.display = "none"; // Hide spinner
+    buttonText.textContent = "Infer"; // Reset button text
+  });
 
-    const response = await fetch("/api/infer_piranha", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
+  // To start the inference, we can emit the 'infer_piranha' event.
+  document
+    .getElementById("submit-button")
+    .addEventListener("click", async function () {
+      const sourceCode = codeBefore.getValue();
+      const targetCode = codeAfter.getValue();
+      const userExplanation = explanationEditor.getValue();
+      const language = languageSelect.value;
+
+      // We don't need to worry about disabling the button or showing a spinner,
+      // since we can now update the interface in real-time as the inference happens.
+      socket.emit("infer_piranha", {
         source_code: sourceCode,
         target_code: targetCode,
         language: language,
         hints: userExplanation,
-      }),
+      });
+
+      // Change the button to show that processing is happening
+      const button = document.getElementById("submit-button");
+      const buttonText = document.getElementById("button-text");
+      const spinner = document.getElementById("spinner");
+
+      button.disabled = true; // Disable button
+      spinner.style.display = "inline-block"; // Show spinner
+      buttonText.textContent = "Processing..."; // Change button text
     });
-
-    // Revert the button to its original state
-    button.disabled = false; // Enable button
-    spinner.style.display = 'none'; // Hide spinner
-    buttonText.textContent = 'Infer'; // Reset button text
-
-    if (response.ok) {
-      const data = await response.json();
-      const toml = data[1];
-      document.getElementById('query-container').style.display = 'block';
-      queryEditor.setValue(toml);
-    } else {
-      console.error("Error:", response.status, response.statusText);
-    }
-  });
 })();
