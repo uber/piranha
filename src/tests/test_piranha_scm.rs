3c172f1f9b01d862970fb0b12dc493e95b7905fd
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-06-30 12:53:54 -0700

    Add Support for tree-sitter queries as a ;anguage

---
+++ src/tests/test_piranha_scm.rs
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2023 Uber Technologies, Inc.
+
+ <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ except in compliance with the License. You may obtain a copy of the License at
+ <p>http://www.apache.org/licenses/LICENSE-2.0
+
+ <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied. See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+use crate::models::default_configs::TS_SCHEME;
+
+use super::create_rewrite_tests;
+
+create_rewrite_tests! {
+  TS_SCHEME,
+  test_simple_rename: "simple_rename", 1;
+
+}
