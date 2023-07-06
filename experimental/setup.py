427fd9cc48f91605271f0712d3080067bf4b3c22
Daniel Ramos <danielrr@uber.com>
2023-07-06 16:14:52 -0700

    Update setup.py

--- experimental/setup.py
+++ experimental/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="MyPackageName",
+    name="piranha-playground",
     version="0.1",
     packages=find_packages(),
     install_requires=[
