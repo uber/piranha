65079cedcd842ab69fe95904a32ee2afae2b47c5
Daniel Ramos <danielrr@uber.com>
2023-07-06 15:59:12 -0700

    Remove bad licenses

--- experimental/rule_inference/piranha_agent.py
+++ experimental/rule_inference/piranha_agent.py
@@ -1,16 +1,3 @@
-/*
-  Copyright (c) 2023 Uber Technologies, Inc.
- 
-  <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
-  except in compliance with the License. You may obtain a copy of the License at
-  <p>http://www.apache.org/licenses/LICENSE-2.0
- 
-  <p>Unless required by applicable law or agreed to in writing, software distributed under the
-  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-  express or implied. See the License for the specific language governing permissions and
-  limitations under the License.
- */
-
 import copy
 import difflib
 import logging
@@ -22,17 +9,23 @@ import attr
 import toml
 from experimental.rule_inference.controller import Controller
 from experimental.rule_inference.graph_parser import GraphParser
-from experimental.rule_inference.piranha_chat import (PiranhaChatException,
-                                                      PiranhaGPTChat)
-from experimental.rule_inference.rule_application import \
-    run_piranha_with_timeout
+from experimental.rule_inference.piranha_chat import (
+    PiranhaChatException,
+    PiranhaGPTChat,
+)
+from experimental.rule_inference.rule_application import run_piranha_with_timeout
 from experimental.rule_inference.static_inference import Inference, QueryWriter
 from experimental.rule_inference.utils.logger_formatter import CustomFormatter
 from experimental.rule_inference.utils.node_utils import NodeUtils
 from experimental.rule_inference.utils.pretty_toml import PrettyTOML
 from experimental.rule_inference.utils.rule_utils import RawRuleGraph
-from polyglot_piranha import (PiranhaArguments, PiranhaOutputSummary, Rule,
-                              RuleGraph, execute_piranha)
+from polyglot_piranha import (
+    PiranhaArguments,
+    PiranhaOutputSummary,
+    Rule,
+    RuleGraph,
+    execute_piranha,
+)
 from tree_sitter import Tree
 from tree_sitter_languages import get_language, get_parser
 
--- experimental/rule_inference/rule_application.py
+++ experimental/rule_inference/rule_application.py
@@ -1,16 +1,3 @@
-/*
-  Copyright (c) 2023 Uber Technologies, Inc.
- 
-  <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
-  except in compliance with the License. You may obtain a copy of the License at
-  <p>http://www.apache.org/licenses/LICENSE-2.0
- 
-  <p>Unless required by applicable law or agreed to in writing, software distributed under the
-  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-  express or implied. See the License for the specific language governing permissions and
-  limitations under the License.
- */
-
 import logging
 import multiprocessing
 from typing import List, Optional, Tuple
@@ -19,8 +6,13 @@ import attr
 import toml
 from experimental.rule_inference.utils.logger_formatter import CustomFormatter
 from experimental.rule_inference.utils.rule_utils import RawRuleGraph
-from polyglot_piranha import (PiranhaArguments, PiranhaOutputSummary, Rule,
-                              RuleGraph, execute_piranha)
+from polyglot_piranha import (
+    PiranhaArguments,
+    PiranhaOutputSummary,
+    Rule,
+    RuleGraph,
+    execute_piranha,
+)
 
 logger = logging.getLogger("CodebaseRefactorer")
 logger.setLevel(logging.DEBUG)
