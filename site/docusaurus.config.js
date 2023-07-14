345afdff6e91224531cdd47358f7b4b7d7d951ef
Daniel Ramos <danielrr@uber.com>
2023-07-13 17:35:25 -0700

    remove broken links

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -109,18 +109,9 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
+                href: 'https://discordapp.com/invite/piranha_uber',
+              }
           },
           {
             title: 'More',
