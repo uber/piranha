79d1e24e375f43bf3b952a1fbf7e6b63e60548eb
Daniel Ramos <danielrr@uber.com>
2023-07-20 15:58:41 -0700

    Cargo was right, and i was wrong

--- src/models/concrete_syntax.rs
+++ src/models/concrete_syntax.rs
@@ -1,3 +1,16 @@
+/*
+ Copyright (c) 2023 Uber Technologies, Inc.
+
+ <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ except in compliance with the License. You may obtain a copy of the License at
+ <p>http://www.apache.org/licenses/LICENSE-2.0
+
+ <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied. See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
 use crate::models::matches::Range;
 use derive_builder::Builder;
 use regex::Regex;
@@ -31,7 +44,6 @@ pub(crate) fn get_all_matches_for_metasyntax_aux(
 
   let node = nodes.pop_front().unwrap();
   let code = node.utf8_text(source_code.as_bytes()).unwrap();
-
 
   // In case the template starts with :[var_name], we try match
   if let Some(caps) = re_var.captures(syntx) {
