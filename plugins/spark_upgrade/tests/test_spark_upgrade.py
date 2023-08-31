c2e7b637ea785080acd90d6b4099863e5101f03c
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-08-31 11:24:30 -0700

    Introduce execute piranha strategy

--- plugins/spark_upgrade/tests/test_spark_upgrade.py
+++ plugins/spark_upgrade/tests/test_spark_upgrade.py
@@ -1,9 +1,20 @@
+# Copyright (c) 2023 Uber Technologies, Inc.
+
+# <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of the License at
+# <p>http://www.apache.org/licenses/LICENSE-2.0
+
+# <p>Unless required by applicable law or agreed to in writing, software distributed under the
+# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 from pathlib import Path
 from os import walk
 from tempfile import TemporaryDirectory
 
-from update_calendar_interval import update_CalendarInterval
+from update_calendar_interval import UpdateCalendarInterval
 
 FORMAT = "%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s"
 logging.basicConfig(format=FORMAT)
@@ -20,7 +31,8 @@ def test_update_CalendarInterval():
         logging.info("Here")
         copy_dir(input_codebase, tp)
         logging.info("Here")
-        summary = update_CalendarInterval([tp])
+        update_calendar_interval = UpdateCalendarInterval([tp])
+        summary = update_calendar_interval()
         assert summary is not None
         assert is_as_expected_files(expected_codebase, tp)
 
