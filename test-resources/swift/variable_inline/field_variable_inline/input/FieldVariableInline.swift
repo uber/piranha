c53d9a296e68b3b4bb0526e8c2c6c9f8390c3b5e
Adarsh Narayan Pandey <adarshnarayan0708@gmail.com>
2023-09-26 22:52:30 +0530

    Swift: Fix local variable declaration inside constructor

--- test-resources/swift/variable_inline/field_variable_inline/input/FieldVariableInline.swift
+++ test-resources/swift/variable_inline/field_variable_inline/input/FieldVariableInline.swift
@@ -8,7 +8,6 @@
 // License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the License for the specific language governing permissions and
 // limitations under the License.
-
 class C1 {
   private var a = placeholder_true
 
