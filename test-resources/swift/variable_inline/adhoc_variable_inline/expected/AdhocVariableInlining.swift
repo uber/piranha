907aa9ab801ec7027f934390c7b5e2009ecaae4a
Adarsh Narayan Pandey <adarshnarayan0708@gmail.com>
2023-09-26 22:45:26 +0530

    Swift: restrict field inline to only private variables (#618)
    * Fix Variable Inline issues related to parameter cleanup in adhoc cases
    
    * Working private field restrictions

--- test-resources/swift/variable_inline/adhoc_variable_inline/expected/AdhocVariableInlining.swift
+++ test-resources/swift/variable_inline/adhoc_variable_inline/expected/AdhocVariableInlining.swift
@@ -9,6 +9,7 @@
 // express or implied. See the License for the specific language governing permissions and
 // limitations under the License.
 class C1{
+    var a = true
     func f1(){
         f.subscribe(
             onNext: { x in 
@@ -23,10 +24,108 @@ class C2{
 
     init(){
         if something{
-            a = true
-            doSomething()
+            var b = true
+            if b {
+                a = true
+                doSomething()
+            }
         } else {
             a = false
+        }
+    }
+
+    func f2a(){
+        if something{
+            
+            a2 = true
+                doSomethingElse()
+            
+        }
+    }
+}
+
+class C3 {
+    private lazy var someVar: SomeType = {
+        let a = false
+        let b: SomeContextType = a ? .caseA : .caseB
+        let c = SomeNavigationParent.buildComboView(for: b, anotherFor: someOtherVar)
+        c.someAttribute = true
+        return c
+    }()
+
+    private var someComputedVar: SomeVarType {
+        let localVar = false
+        return shared {
+            SomeFunctionCall(a: localVar ? "a" : "b", c: c)
+        }
+    }
+
+    
+
+    private func f3() -> String{
+        abc.subscribe(onNext: {(someVar: SomeVarType) in
+        switch someVar {
+            case .caseA:
+            return "someString"
+            case .caseB: 
+            return "someOtherString"
+            case .caseC:
+             return "to_be_retained"
+            case .caseD:
+             return "to_be_retained"
+
+            default: "another_test_case"
+        }})
+    }
+
+    private func f4() -> String{
+        abc.subscribe(onNext: {(someVar: SomeVarType) in
+            if someCondition {
+                doSomeCalls()
+            } else {
+                let someInternalVar = "some_internal_var"
+                if someOtherCondition {
+                    someFunctionCall()
+                    
+                }
+            }
+        })
+    }
+}
+
+class C4 {
+     
+    var varA: A 
+    var varB: B
+
+    private lazy var someComputedProperty: SomePropertyType {
+        let a = SomeFunctionCall(
+            firstVar: .a,
+            localVar: false,
+            secondVar: .b,
+            thirdVar: thirdVar
+        )
+    }
+
+    init(varA: A, varB: B){
+        
+        self.varA = varA
+        self.varB = varB
+
+        someFunctionCall(varB, varA, false)
+        
+    }
+
+    func ifInSwitch() -> String?{
+        switch varA {
+            case .a:
+                return "a"
+            case .b:
+                let someNestedVar = "some_nested_var"
+                doSOmethingElse()
+                
+            default:
+                return nil
         }
     }
 }
--- test-resources/swift/variable_inline/adhoc_variable_inline/input/AdhocVariableInlining.swift
+++ test-resources/swift/variable_inline/adhoc_variable_inline/input/AdhocVariableInlining.swift
@@ -37,4 +37,116 @@ class C2{
             a = false
         }
     }
+
+    func f2a(){
+        if something{
+            var b2 = placeholder_true
+            if b2 {
+                a2 = placeholder_true
+                doSomethingElse()
+            }else {
+                a = false
+            }
+        }
+    }
+}
+
+class C3 {
+    private lazy var someVar: SomeType = {
+        let a = placeholder_false
+        let b: SomeContextType = a ? .caseA : .caseB
+        let c = SomeNavigationParent.buildComboView(for: b, anotherFor: someOtherVar)
+        c.someAttribute = true
+        return c
+    }()
+
+    private var someComputedVar: SomeVarType {
+        let localVar = placeholder_false
+        return shared {
+            SomeFunctionCall(a: localVar ? "a" : "b", c: c)
+        }
+    }
+
+    private var x = !placeholder_false
+
+    private func f3() -> String{
+        abc.subscribe(onNext: {(someVar: SomeVarType) in
+        switch someVar {
+            case .caseA:
+            return "someString"
+            case .caseB: 
+            return "someOtherString"
+            case .caseC:
+             if self.x{
+                return "to_be_retained"
+             }
+             return "to_be_deleted"
+            case .caseD:
+                if x{
+                    return "to_be_retained"
+                }
+            return "to_be_deleted"
+            default: "another_test_case"
+        }})
+    }
+
+    private func f4() -> String{
+        abc.subscribe(onNext: {(someVar: SomeVarType) in
+            if someCondition {
+                doSomeCalls()
+            } else {
+                let someInternalVar = "some_internal_var"
+                if someOtherCondition {
+                    if self.x {
+                        someFunctionCall()
+                    } else {
+                        someOtherFunctionCall()
+                    }
+                }
+            }
+        })
+    }
+}
+
+class C4 {
+    private let localVar: Bool 
+    var varA: A 
+    var varB: B
+
+    private lazy var someComputedProperty: SomePropertyType {
+        let a = SomeFunctionCall(
+            firstVar: .a,
+            localVar: localVar,
+            secondVar: .b,
+            thirdVar: thirdVar
+        )
+    }
+
+    init(varA: A, varB: B){
+        self.localVar = placeholder_false
+        self.varA = varA
+        self.varB = varB
+
+        if localVar {
+            someFunctionCall(varA, varB, localVar)
+        } else {
+            someFunctionCall(varB, varA, localVar)
+        }
+    }
+
+    func ifInSwitch() -> String?{
+        switch varA {
+            case .a:
+                return "a"
+            case .b:
+                let someNestedVar = "some_nested_var"
+                if localVar {
+                    doSomething()
+                } else {
+                    doSOmethingElse()
+                }
+            default:
+                return nil
+        }
+    }
 }
