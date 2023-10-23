c44be8ea88c23f928294d4242beddd58b9910d43
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-10-23 08:39:00 -0700

    .

--- plugins/spark_upgrade/spark_config/scala_rules.py
+++ plugins/spark_upgrade/spark_config/scala_rules.py
@@ -1,6 +1,5 @@
 from polyglot_piranha import Rule
 
-
 update_enclosing_var_declaration_scala = Rule(
     name="update_enclosing_var_declaration_scala",
     query="cs val :[conf_var] = :[rhs]",
@@ -19,6 +18,5 @@ update_spark_context_scala = Rule(
     is_seed_rule=False,
     groups={"update_spark_context"},
 )
-
 
 RULES = [update_enclosing_var_declaration_scala, update_spark_context_scala]
