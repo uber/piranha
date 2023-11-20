d0cfa9a6f797a9bc0f4ab4d7c26e693022720ef7
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-11-19 20:30:09 -0800

    .

--- plugins/spark_upgrade/tests/resources/spark_conf/input/sample.java
+++ plugins/spark_upgrade/tests/resources/spark_conf/input/sample.java
@@ -3,27 +3,28 @@ package com.piranha;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.SparkSession;
+import org.spark_project.jetty.util.ArrayQueue;
 
 public class Sample {
     public static void main(String[] args) {
         String master = "local";
         String appName = "SampleApp";
         String sparkHome = "your_spark_home_directory";
-        
+
         SparkConf conf = new SparkConf()
                 .setMaster(master)
                 .setAppName(appName)
                 .set("spark.driver.allowMultipleContexts", "true");
-        
+
         SparkContext sc = new SparkContext(conf);
         TestHiveContext sqlContext = new TestHiveContext(sc);
         SparkSession sparkSession = sqlContext.sparkSession();
 
         SparkConf conf2 = new SparkConf();
         conf2.setSparkHome(sparkHome);
-        
+
         conf2.setExecutorEnv("spark.executor.extraClassPath", "test");
-        
+
         SparkSession sparkSession = SparkSession.builder()
                 .master(master)
                 .appName(appName)
