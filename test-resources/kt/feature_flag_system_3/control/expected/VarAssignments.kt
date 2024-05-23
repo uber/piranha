4c29d23a1e273d6a41d90e235eca7c39f1e447d1
aleksandr.zakshevskii <aleksandr.zakshevskii@n26.com>
2024-05-23 16:31:55 +0200

    test cases for the rules generated

---
+++ test-resources/kt/feature_flag_system_3/control/expected/VarAssignments.kt
@@ -0,0 +1,13 @@
+enum class FeatureFlags {
+    FEATURE_B
+    ;
+}
+
+class Sample {
+
+    fun someMethod() {
+        println("Hello world!")
+        println("Hi world!")
+        println("Hi world!")
+    }
+}
