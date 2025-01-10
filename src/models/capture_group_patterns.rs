ccf32579238643cfb95ea93218d66d60fe9b6142
Corbin McElhanney <corbin.mcelhanney@snowflake.com>
2025-01-09 16:46:13 -0800

    Add CGPattern pattern getter

--- src/models/capture_group_patterns.rs
+++ src/models/capture_group_patterns.rs
@@ -21,7 +21,7 @@ use crate::{
     Instantiate,
   },
 };
-use pyo3::prelude::pyclass;
+use pyo3::prelude::{pyclass, pymethods};
 use regex::Regex;
 use serde_derive::Deserialize;
 use std::collections::HashMap;
@@ -43,11 +43,13 @@ pub enum PatternType {
 #[derive(Deserialize, Debug, Clone, Default, PartialEq, Hash, Eq)]
 pub struct CGPattern(pub String);
 
+#[pymethods]
 impl CGPattern {
   pub(crate) fn new(query: String) -> Self {
     Self(query)
   }
 
+  #[getter]
   pub(crate) fn pattern(&self) -> String {
     self.0.to_string()
   }
