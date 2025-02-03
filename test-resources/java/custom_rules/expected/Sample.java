6d14da9bdd4229af0d821c797e10fc6ced6a548e
norhh <nikhilparasaram@gmail.com>
2025-02-03 19:30:00 +0100

    Add python tests

---
+++ test-resources/java/custom_rules/expected/Sample.java
@@ -0,0 +1,11 @@
+public class PokemonCatcher {
+    private void shouldNotBeDeleted_4() {
+        if (sheeps) {
+        }
+        else if (check){
+            blah = 0;
+        }
+        else {
+        }
+    }
+}
